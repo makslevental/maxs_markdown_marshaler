aa1e14bbcef5c5dcdd80d4733546ce1f639d7485
maksim <maksim.levental@gmail.com>
2016-12-07 21:06:23 -0500

    update to use html template

--- compile.py
+++ compile.py
@@ -23,32 +23,26 @@ class GUI:
         self.font = Font(family="Helvetica", size=12)
         self.markdown_file_button = Button(self.app, text='Markdown File', command=self.pick_file, font=self.font)
         self.markdown_file_button.grid(row=0,column=0, columnspan=2)
-        self.css_file_button = Button(self.app, text='CSS File', command=self.pick_css,font=self.font)
-        self.css_file_button.grid(row=1,column=0,columnspan=2)
-        
-        
-        
+        self.html_template_file_button = Button(self.app, text='HTML Template', command=self.pick_html_template, font=self.font)
+        self.html_template_file_button.grid(row=1,column=0,columnspan=2)
+
         self.v = StringVar()
         self.v.set("Chrome") # initialize
         Radiobutton(self.app, text="Chrome", variable=self.v, value="Chrome").grid(row=2, column=0)        
         Radiobutton(self.app, text="Firefox", variable=self.v, value="Firefox").grid(row=2, column=1)
         self.start_button = Button(self.app, text='Start', command=self.start, font=self.font)
         self.start_button.grid(row=3,column=0, columnspan=4)
-        
-        self.html_start = '<!doctype HTML><html><head><meta charset="utf-8"><title>Made with Max\'s Markdown Marshaler!</title>'
-        self.html_middle2 = "</head><body id='MathPreviewF'>"
-        self.html_end = '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.6.0/styles/default.min.css"><script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.6.0/highlight.min.js"></script><script>hljs.initHighlightingOnLoad();</script><script type="text/javascript" src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script><script type="text/javascript">MathJax.Hub.Config({"showProcessingMessages" : false,"messageStyle" : "none","tex2jax": { inlineMath: [ [ "$", "$" ] ] }});</script></body></html>'
     
     def pick_file(self):
         self.markdown_file_path = askopenfilename()
         self.markdown_file_button.config(text=os.path.basename(self.markdown_file_path))
         self.html_file_path = os.path.dirname(self.markdown_file_path)+os.sep+os.path.splitext(os.path.basename(self.markdown_file_path))[0]+'.html'
         
-    def pick_css(self):
-        self.css_file_path = askopenfilename()
-        with open(self.css_file_path) as css:
-            self.html_middle1 = "<style type='text/css'>"+css.read()+"</style>"
-        self.css_file_button.config(text=os.path.basename(self.css_file_path))
+    def pick_html_template(self):
+        self.html_template_file_path = askopenfilename()
+        with open(self.html_template_file_path) as html:
+            self.html = html.read()
+        self.html_template_file_button.config(text=os.path.basename(self.html_template_file_path))
     
     def start(self):
         
@@ -69,8 +63,8 @@ class GUI:
     
     def write_to_html(self):
         text = open(self.markdown_file_path,"r").read()
-        html_middle3 = markdown.markdown(text, default_extensions)
-        html = self.html_start + self.html_middle1 + self.html_middle2 + html_middle3 + self.html_end
+        markdown_html = markdown.markdown(text, default_extensions)
+        html = self.html.format(html=markdown_html)
         tf = open(self.html_file_path,'w+b')
         tf.write(html.encode())
         tf.flush()
@@ -94,11 +88,15 @@ class GUI:
             if event.src_path == self.fp:
                 self.render_fn()
                 self.driver.refresh()
-                time.sleep(4)
-                self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight+100);")
+                for i in range(10):
+                    time.sleep(1)
+                    self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight+100);")
         
     def mainloop(self):
         self.app.mainloop()
 
 if __name__ == '__main__':
     GUI().mainloop()
+    #driver = webdriver.Firefox()
+    #driver.get('http://lkqpickyourpart.com/locations')
+    #print(driver.execute_script('return window.maxs_markers'))
